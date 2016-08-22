5bc981d95f089488639057edd465c352a227618a
Daniel Hu <hdc@pantum.com>
2016-08-22 20:53:51 +0800

    init

---
+++ app/src/test/java/com/pantum/m/wifip2pdemo/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.pantum.m.wifip2pdemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
